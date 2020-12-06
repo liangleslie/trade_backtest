75adf98b2ca465ffb36fb93b788231a55441747a
leslieliang <liang.leslie@gmail.com>
2020-12-07 04:04:37 +0800

    data download from yahoo finance

--- backtest.js
+++ backtest.js
@@ -1,25 +1,25 @@
 var bench_ticker = "SPY";
 var data_tickers = ["QQQ","IJJ","SPY"];
+const cors_proxy = "https://cors-anywhere.herokuapp.com/";
+var data = []
+
 data_tickers.push(bench_ticker);
 data_tickers = data_tickers.filter(onlyUnique);
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
-// for (var ticker of data_tickers) {
-//     var url = "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?interval=1d&range=30y"
-//     fetch(url, {mode: 'no-cors'}) // temporarily enabling 'no-cors'
-//         .then(
-//             response => response.text() // .json(), etc.
-//             // same as function(response) {return response.text();}
-//         ).then(
-//             html => console.log(html)
-//         );
-// }
 
-const url = "https://query1.finance.yahoo.com/v8/finance/chart/SPY?interval=1d&range=30y"
-fetch(url).then(
-        response => response.json()
-    ).then(
-        data => console.log(data)
-    );
+
+// download and store latest tickers from yahoo finance
+for (var ticker of data_tickers) {
+    var url = "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?interval=1d&range=30y"
+    fetch(cors_proxy+url, {headers: {origin: ""}}) // origin needed by cors_proxy
+        .then(
+            response => response.json()
+        ).then(
+            response_data => {
+                data[response_data["chart"]["result"][0]["meta"]["symbol"]] = response_data;
+            }
+        );
+}

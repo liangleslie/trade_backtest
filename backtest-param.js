dacaa16abe37f271a62e724c64e2d90e27894d5f
Leslie Liang <liang.leslie@gmail.com>
2020-12-12 05:40:41 +0800

    implemented parameter input and updating

--- backtest-param.js
+++ backtest-param.js
@@ -1,20 +1,74 @@
-var instrumentParams = [{
+/* 
+Input parameter definitions
+ */
+
+var instrumentParams = {
+	key: "Instrument Parameters", collapsible: true, children:[{
 	key: "Instruments", collapsible: true, children: [{
-		key: "Super bull instrument", collapsible: false, defaultValue: "SPXL"
+		key: "Super bull instrument", defaultValue: "SPXL"
 	},{
-		key: "Bull instruments", collapsible: false, defaultValue: "QQQ,SPY,IJJ"
-	}]},
-	{key: "Param Name", defaultValue: "Param Value"}
-];
+		key: "Bull instruments", defaultValue: "QQQ,SPY,IJJ"
+	},{
+		key: "Bear instruments", defaultValue: "GLD,XLP"
+	},{
+		key: "Safe stock", defaultValue: "TLT"
+	},{
+		key: "Short", defaultValue: "VXX"
+}]},{
+	key: "Thresholds", collapsible: true, children: [{
+		key: "Super bull threshold", defaultValue: "80%"
+	},{
+		key: "Bull threshold", defaultValue: "60%"
+	},{
+		key: "Bear threshold", defaultValue: "40%"
+	},{
+		key: "Short threshold", defaultValue: "20%"
+}]},{
+	key: "Other parameters", collapsible: true, children: [{
+		key: "Slippage", defaultValue: "0.1%"
+	},{
+		key: "Rebalance frequency", defaultValue: "999"
+	},{
+		key: "Start date", defaultValue: "01/01/2001"
+	},{
+		key: "End date", defaultValue: "12/31/2020"
+}]}]};
+
+var indicatorParams = {
+	key: "Indicator Parameters", collapsible: true, children:[{
+	key: "Indicator weights", collapsible: true, children: [{
+		key: "Momentum", defaultValue: "1%"
+	},{
+		key: "High-Low SMA", defaultValue: "1%"
+	},{
+		key: "Inflation", defaultValue: "1%"
+	},{
+		key: "VIX", defaultValue: "1%"
+	},{
+		key: "Unemployment", defaultValue: "1%"
+	},{
+		key: "S&P Beta", defaultValue: "1%"
+	}]
+}]};
+
+var metaParam = {
+	key: "Inputs", collapsible: true, children: [instrumentParams,indicatorParams]
+}
+
+
+/* 
+Functions start here
+ */
+
 
 //build nested table from key
-
 function buildTable(params, depth = 0) {
 	var htmlOutput = "";
+	var idFromName = params.key.toLowerCase().replaceAll(" ","-");
 	var currentClassName = "collapse level"+depth;
 	htmlOutput += `<tr data-depth="${ depth }" class="${ currentClassName }">
-<td>${ params.collapsible ? `<span class="toggle collapse"></span>` : "" }${ params.key }</td>
-${ "defaultValue" in params ? `<td>${ params.defaultValue }</td>` : ""}</tr>
+<td><div class="param">${ params.collapsible ? `<span class="toggle collapse"></span>` : "" }${ params.key }</div></td>
+${ "defaultValue" in params ? `<td><input class="inputData" type="text" id="${ idFromName }" name="${ idFromName }" value="${ params.defaultValue }"></td>` : ""}</tr>
 `;
 	if ("children" in params) {
 		for (childParams of params.children) {htmlOutput += buildTable(childParams, depth+1)}
@@ -22,8 +76,16 @@ ${ "defaultValue" in params ? `<td>${ params.defaultValue }</td>` : ""}</tr>
 	return htmlOutput;
 };
 
+//pull input params into inputObject
+function pullInputParams() {
+	var inputObject = {};
+	inputNodes = document.querySelectorAll(".inputData");
+	for (node of inputNodes) {
+		inputObject[node.name] = node.value
+	};
+	return inputObject;
+};
 
-tableContents = buildTable({
-	key: "Instrument Parameters", collapsible: true, children: instrumentParams
-});
-document.getElementById("paramTable").innerHTML = tableContents
+
+tableContents = buildTable(metaParam);
+document.getElementById("paramTable").innerHTML = tableContents;
--- main.js
+++ main.js
@@ -2,11 +2,66 @@
 Variables defined here
 Should be temporary - variables should be defined and modified from HTML
 */
+var inputObject = pullInputParams();
 var bench_ticker = "SPY";
 var data_tickers = ["QQQ","IJJ","SPY"];
 const cors_proxy = "https://cors-anywhere.herokuapp.com/";
+var chartStartDate = new Date(inputObject["start-date"]);
+
+
+/*
+Chart related variables go here
+*/
 const ctx = document.getElementById('myChart').getContext('2d');
-var chartStartDate = new Date("1/1/2001")
+var chartDataArray = [];
+
+var chartOptions = function(chartStartDate) {
+	return {
+		elements: {
+			point: {
+				radius: 0
+			}
+		},
+		events: ['click'],
+		tooltips: {
+			mode: "nearest",
+			intersect: false
+		},
+		animation: {
+			duration: 0 // general animation time
+		},
+		hover: {
+			animationDuration: 0 // duration of animations when hovering an item
+		},
+		responsiveAnimationDuration: 0, // animation duration after a resize
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}],
+			xAxes: [{
+				type: 'time',
+				ticks: {
+					min: chartStartDate // set start date
+				},
+				time: {
+					unit: 'month'
+				}
+			}]
+		}
+	};
+}
+
+var chartContents = {
+		type: 'line',
+		data: {
+			datasets: chartDataArray
+		},
+		options: chartOptions(chartStartDate)
+};
+
+
 
 /*
 functions defined here
@@ -15,6 +70,7 @@ function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
+//create series object to store quote and indicator data
 function series(dates,dataObject) {
 	this.dates = dates;
 	this.first_date = Math.min(... this.dates);
@@ -56,53 +112,9 @@ function series(dates,dataObject) {
 		// console.log(date,requestDate,this.dates[dateIndex],dateIndex,dataRequest);  // for debug purposes
 		return dataRequest;
 	};
-}
-
-var buildChartContents = function(chartDataArray, chartStartDate) {
-	return ({
-		type: 'line',
-		data: {
-			datasets: chartDataArray // chartData goes here
-		},
-		options: {
-			elements: {
-				point: {
-					radius: 0
-				}
-			},
-			events: ['click'],
-			tooltips: {
-				mode: "nearest",
-				intersect: false
-			},
-			animation: {
-				duration: 0 // general animation time
-			},
-			hover: {
-				animationDuration: 0 // duration of animations when hovering an item
-			},
-			responsiveAnimationDuration: 0, // animation duration after a resize
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
-					}
-				}],
-				xAxes: [{
-					type: 'time',
-					ticks: {
-						min: chartStartDate // set start date
-					},
-					time: {
-						unit: 'month'
-					}
-				}]
-			}
-		}
-	})
 };
 
-var addToChartDataArray = function (chartData, label, chartDataArray, color = "rgba(255, 99, 132, 0.2)") {
+var addToChartDataArray = function (chartData, label, chartDataArray, color) {
 	chartDataArray.push({
 		label: label,
 		data: chartData,
@@ -112,63 +124,79 @@ var addToChartDataArray = function (chartData, label, chartDataArray, color = "r
 		fill: false
 	});
 	return chartDataArray;
-}
-
-
-//
-
+};
 
 
 
 /*
 Execution starts here
 */
+function startExec() {
+	// download and store latest tickers from yahoo finance
+	var quotes = [];
+	var promises = [];
+	data_tickers.push(bench_ticker);
+	data_tickers = data_tickers.filter(onlyUnique);
 
-// download and store latest tickers from yahoo finance
-var quotes = [];
-var chartDataArray = [];
-var promises = [];
-data_tickers.push(bench_ticker);
-data_tickers = data_tickers.filter(onlyUnique);
+/* 	temporarily disabled to use offline data
+	for (var ticker of data_tickers) {
+		var url = "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?interval=1d&range=30y";
+		promises.push(fetch(cors_proxy+url, {headers: {origin: ""}}) // origin needed by cors_proxy
+			.then(
+				response => response.json()
+			).then(
+				response_data => {
+					dates = response_data['chart']['result'][0]['timestamp'].map(x => new Date((x-x%(60*60*24))*1000));
+					dataObject = {
+						'close': response_data['chart']['result'][0]["indicators"]["quote"][0]['close'],
+						'open': response_data['chart']['result'][0]["indicators"]["quote"][0]['open'],
+						'high': response_data['chart']['result'][0]["indicators"]["quote"][0]['high'],
+						'low': response_data['chart']['result'][0]["indicators"]["quote"][0]['low'],
+						'volume': response_data['chart']['result'][0]["indicators"]["quote"][0]['volume']
+					};
+					quotes[response_data["chart"]["result"][0]["meta"]["symbol"]] = new series(dates,dataObject);
+				}
+			)
+		)
+	}
+*/
 
-for (var ticker of data_tickers) {
-    var url = "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?interval=1d&range=30y";
-    promises.push(fetch(cors_proxy+url, {headers: {origin: ""}}) // origin needed by cors_proxy
-        .then(
-            response => response.json()
-        ).then(
-            response_data => {
-				dates = response_data['chart']['result'][0]['timestamp'].map(x => new Date((x-x%(60*60*24))*1000));
-				dataObject = {
-					'close': response_data['chart']['result'][0]["indicators"]["quote"][0]['close'],
-					'open': response_data['chart']['result'][0]["indicators"]["quote"][0]['open'],
-					'high': response_data['chart']['result'][0]["indicators"]["quote"][0]['high'],
-					'low': response_data['chart']['result'][0]["indicators"]["quote"][0]['low'],
-					'volume': response_data['chart']['result'][0]["indicators"]["quote"][0]['volume']
-				};
-                quotes[response_data["chart"]["result"][0]["meta"]["symbol"]] = new series(dates,dataObject);
-            }
-        )
-	)
-}
+// temp block for offline json loading
+	for (var ticker of [IJJ,SPY,QQQ]) {
+		dates = ticker['chart']['result'][0]['timestamp'].map(x => new Date((x-x%(60*60*24))*1000));
+		dataObject = {
+			'close': ticker['chart']['result'][0]["indicators"]["quote"][0]['close'],
+			'open': ticker['chart']['result'][0]["indicators"]["quote"][0]['open'],
+			'high': ticker['chart']['result'][0]["indicators"]["quote"][0]['high'],
+			'low': ticker['chart']['result'][0]["indicators"]["quote"][0]['low'],
+			'volume': ticker['chart']['result'][0]["indicators"]["quote"][0]['volume']
+		};
+		quotes[ticker["chart"]["result"][0]["meta"]["symbol"]] = new series(dates,dataObject);	
+	}
+		
+	
+	// build chartData using backtest logic after all data is downloaded
 
+	//build chart temp
+	Promise.all(promises).then(x=> {
+		chartDataArray = [];
+		chartDataArray = addToChartDataArray(quotes.IJJ.closeChartData,"IJJ",chartDataArray, "rgba(255, 99, 132, 0.5)");
+		chartDataArray = addToChartDataArray(quotes.SPY.closeChartData,"SPY",chartDataArray, "rgba(99, 255, 132, 0.5)");
+		chartDataArray = addToChartDataArray(quotes.QQQ.closeChartData,"QQQ",chartDataArray, "rgba(99, 132, 255, 0.5)");
+		chartContents.data.datasets = chartDataArray;
+	})
+	.then(x=> {
+		myChart = new Chart(ctx, chartContents) //build chart into html
+	});
+};
 
+function updateExec() {
+	inputObject = pullInputParams();
+	console.log(inputObject);
+	// update backtest and buildChartContents
+	myChart.options = chartOptions(new Date(inputObject["start-date"])); // update options including start date
+	myChart.data.datasets = chartDataArray; // update chart data on myChart
+	myChart.update()
+};
 
-//display model properties in a table
-
-//allow modifiable parameters
-
-//build backtest logic and rules
-
-
-
-
-//build chartData after all data is downloaded
-Promise.all(promises).then(x=> {
-	chartDataArray = addToChartDataArray(quotes.IJJ.closeChartData,"IJJ",chartDataArray)
-	chartDataArray = addToChartDataArray(quotes.SPY.closeChartData,"SPY",chartDataArray, "rgba(99, 255, 132, 0.2)")
-	chartDataArray = addToChartDataArray(quotes.QQQ.closeChartData,"QQQ",chartDataArray, "rgba(99, 132, 255, 0.2)")
-})
-.then(x=> {
-	myChart = new Chart(ctx, buildChartContents(chartDataArray, chartStartDate)) //build chart into html
-});
+startExec();
